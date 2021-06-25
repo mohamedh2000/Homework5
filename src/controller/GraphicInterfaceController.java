30889f15fca0305d75d73554468bb0046fc26ca7
Spencer Soodek <spencersoodek@gmail.com>
2021-06-25 14:21:24 -0500

    WIP gui controller

--- src/controller/GraphicInterfaceController.java
+++ src/controller/GraphicInterfaceController.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import model.Project;
-import model.filtermodel.Blur;
 import view.GraphicInterface;
 
 public class GraphicInterfaceController implements ActionListener {
